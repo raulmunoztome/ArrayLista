552c9a6a2946cc3af9fd788144988367d13094a6
cfgs <cfgs@pc-info-host>
2026-01-28 21:07:46 +0100

    Práctica ArrayList

---
+++ Listas/src/ArrayLista.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayLista {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> colors = new ArrayList<>();
+		
+	colors.add("Verde");
+	colors.add("Amarillo");
+	
+	//foreach para mostrarlo.
+		for(String elemento : colors) {
+			System.out.println(elemento);
+		}
+		LinkedList<String> colors2 = new LinkedList<>();
+		
+	
+	
+	}
+	
+
+}
