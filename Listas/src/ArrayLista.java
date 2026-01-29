e727948432dcbeed6bdf3eee24ced37a61ac514c
raulyypunto <raul_stb@hotmail.com>
2026-01-29 02:51:08 +0100

    Termino el ejercicio añadiendo los ejemplos restantes

--- Listas/src/ArrayLista.java
+++ Listas/src/ArrayLista.java
@@ -7,15 +7,37 @@ public class ArrayLista {
 		
 		ArrayList<String> colors = new ArrayList<>();
 		
-	colors.add("Verde");
-	colors.add("Amarillo");
+		colors.add("Verde");
+		colors.add("Amarillo");
 	
-	//foreach para mostrarlo.
 		for(String elemento : colors) {
-			System.out.println(elemento);
+			System.out.print(elemento+", ");
 		}
+		System.out.println("tamaño del arraylist: "+colors.size());
+		
 		LinkedList<String> colors2 = new LinkedList<>();
 		
+		colors2.add("Fosforito");
+		colors2.add("brilli brilli");
+		colors2.add("Rosa");
+		
+		for(String elemento2 : colors2) {
+			System.out.print(elemento2+", ");
+		}
+		System.out.println();
+		System.out.println("Tamaño del LinkedList: "+colors2.size());
+		
+		String[] colors3 = new String[10];
+		
+		colors3[0] = "Rojo PSOE";
+		colors3[1] = "Verde esperanza";
+		
+		for(String element : colors3) {
+			System.out.print(element+", ");
+
+		}
+		System.out.println();
+		System.out.println("Tamañao del array: "+colors3.length);
 	
 	
 	}
